7be4165948bd1c3d19462a91326d3f119bba17cb
aqs_coder <aqsuel345@gmail.com>
2026-03-25 23:36:07 -0300

    Aula 571, Criando o model do perfil de usuários

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,74 @@
+import re
+
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+from utils.validacpf import valida_cpf
+
+
+class Perfil(models.Model):
+    class Meta:
+        verbose_name = "Perfil"
+        verbose_name_plural = "Perfis"
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+
+    # TODO: Mudar o endereço para um model exclusivo
+    endereco = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+
+    # TODO: Fazer com que o cep preencha a cidade e estado automaticamente
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        choices=(
+            ("AC", "Acre"),
+            ("AL", "Alagoas"),
+            ("AP", "Amapá"),
+            ("AM", "Amazonas"),
+            ("BA", "Bahia"),
+            ("CE", "Ceará"),
+            ("DF", "Distrito Federal"),
+            ("ES", "Espírito Santo"),
+            ("GO", "Goiás"),
+            ("MA", "Maranhão"),
+            ("MT", "Mato Grosso"),
+            ("MS", "Mato Grosso do Sul"),
+            ("MG", "Minas Gerais"),
+            ("PA", "Pará"),
+            ("PB", "Paraíba"),
+            ("PR", "Paraná"),
+            ("PE", "Pernambuco"),
+            ("PI", "Piauí"),
+            ("RJ", "Rio de Janeiro"),
+            ("RN", "Rio Grande do Norte"),
+            ("RS", "Rio Grande do Sul"),
+            ("RO", "Rondônia"),
+            ("RR", "Roraima"),
+            ("SC", "Santa Catarina"),
+            ("SP", "São Paulo"),
+            ("SE", "Sergipe"),
+            ("TO", "Tocantins")
+        )
+    )
+
+    def clean(self):
+        mensagens_erro = {}
+
+        if not valida_cpf(self.cpf):
+            mensagens_erro['cpf'] = "CPF inválido"
+
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            mensagens_erro['cep'] = "CEP inválido"
+
+        if mensagens_erro:
+            raise ValidationError(mensagens_erro)
+
+    def __str__(self):
+        return f'{self.usuario}'
---
+++ runserver.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "📂 Coletando arquivos estáticos..." -ForegroundColor Cyan
+python manage.py collectstatic --noinput
+
+Write-Host "⚙️ Preparando as migrações..." -ForegroundColor Cyan
+python manage.py makemigrations --noinput
+
+Write-Host "🗄️ Aplicando as migrações..." -ForegroundColor Cyan
+python manage.py migrate --noinput
+
+Write-Host "🚀 Iniciando o servidor local..." -ForegroundColor Green
+python manage.py runserver
